b197ee514d471614c4a9c7634625412a23730dea
dayaveer1 <dayaveerb@gmail.com>
2025-07-03 10:58:53 +0100

    Reorganise file structure

--- data/collector/__innit__.py
+++
@@ -1,9 +0,0 @@
-# data/collectors/__init__.py
-"""
-Data collectors package for S&P 500 futures trading system
-"""
-
-from .futures_collector import ESFuturesCollector
-
-# Makes imports cleaner
-__all__ = ["ESFuturesCollector"]
